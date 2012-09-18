05c5c180bde57899cc0652de7bca5967b5d7ff36
DataMinerUK <nicolarhughes@gmail.com>
2012-09-18 22:02:27 +0100

    Add RStudio to vagrant.

--- vagrant/manifests/scientific.pp
+++ vagrant/manifests/scientific.pp
@@ -4,8 +4,16 @@ class scientific {
     [   
       gnuplot,
       octave,
-      r-base
+      r-base,
+      libjpeg62
     ]: ensure => latest;
   }  
   
+  exec {
+    rstudio:
+      command => '/usr/bin/wget http://download1.rstudio.org/rstudio-0.96.331-amd64.deb && dpkg -i rstudio-0.96.331-amd64.deb',
+      creates => '/usr/bin/rstudio';
+  }
+
+  Package[libjpeg62] -> Exec[rstudio]
 }
