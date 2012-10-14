5ec5fbc6ef60bc9b242a1ef25276f820d9d45325
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-10-14 19:19:43 +0100

    Fix up MySQL bindaddress to be available from host.

--- vagrant/manifests/db.pp
+++ vagrant/manifests/db.pp
@@ -9,7 +9,13 @@ class db {
 
   class { 'mysql': }
   class { 'mysql::python': }
-  class { 'mysql::server': config_hash => { 'root_password' => 'password' } }
+  class {
+    'mysql::server':
+      config_hash => {
+        'bind_address'  => '0.0.0.0',
+        'root_password' => 'password'
+    }
+  }
 
   database_grant { 'root@%/*.*': privileges => ['all'] }
 
