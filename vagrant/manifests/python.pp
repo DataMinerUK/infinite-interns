71f63e2c1a8e3ba4ed2ba19b1314fcf71cba7964
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-10-17 11:10:15 +0100

    Add ipython-notebook.

--- vagrant/manifests/python.pp
+++ vagrant/manifests/python.pp
@@ -4,33 +4,9 @@ class python {
   package {
     [
       'ipython',
+      'ipython-notebook',
       'python-setuptools',
-      'python-bs4',
-      'python-chardet',
-      'python-dateutil',
-      'python-enchant',
-      'python-facebook',
-      'python-feedparser',
-      'python-fs',
-      'python-gdata',
-      'python-geohash',
-      'python-geoip',
-      'python-gps',
-      'python-html2text',
-      'python-htmlgen',
-      'python-httplib2',
-      'python-imaging',
-      'python-lxml',
-      'python-libxml2',
-      'python-markdown',
-      'python-markupsafe',
-      'python-prettytable',
-      'python-scour',
-      'python-scrapy',
-      'python-simplejson',
-      'python-tz',
-      'python-urlgrabber',
-      'python-yaml'
+      'python-bs4'
     ]: ensure => installed;
   }
 
