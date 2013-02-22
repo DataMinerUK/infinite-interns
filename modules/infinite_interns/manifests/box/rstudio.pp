63ac2ee21b8c70f1974024940d8c12626eca272c
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2013-02-22 18:42:33 +0000

    Add standard packages and BUGS to r intern.

--- modules/infinite_interns/manifests/box/rstudio.pp
+++ modules/infinite_interns/manifests/box/rstudio.pp
@@ -2,6 +2,8 @@
 # Called rstudio because one letter hostnames are a problem in Vagrant
 class infinite_interns::box::rstudio {
 
+  require bugs
+
   apt::source {
     'cran':
       location    => 'http://cran.ma.imperial.ac.uk/bin/linux/ubuntu',
@@ -16,18 +18,41 @@ class infinite_interns::box::rstudio {
     [
       'gnuplot',
       'r-base',
-      'libjpeg62'
+      'libjpeg62',
+      'texlive',
+      'pandoc',
+      'libcairo2-dev',
+      'build-essential'
     ]: ensure => latest;
   }
 
+  file {
+    '/root/R.setup':
+      source => 'puppet:///modules/rstudio/root/R.setup',
+      owner  => root,
+      group  => root,
+      mode   => '0744';
+
+    # Fixup for JAGS library path
+    '/usr/lib64':
+      ensure => 'link',
+      target => '/usr/lib';
+  }
+
   $url = 'http://download1.rstudio.org'
-  $filename = 'rstudio-0.97.312-amd64.deb'
+  $filename = 'rstudio-0.97.314-amd64.deb'
 
   exec {
     'download-rstudio':
       command => "/usr/bin/wget ${url}/${filename}",
       cwd     => '/root',
       creates => "/root/${filename}",
+      timeout => 0;
+
+    'setup-R':
+      cwd     => '/root',
+      command => '/root/R.setup',
+      creates => '/root/R.done',
       timeout => 0;
   }
 
@@ -38,8 +63,13 @@ class infinite_interns::box::rstudio {
       source   => "/root/${filename}";
   }
 
-  Apt::Source['cran'] -> Package['r-base']
+  Apt::Source['cran'] -> Package['r-base'] -> Exec['setup-R']
 
   Package['libjpeg62'] -> Package['rstudio']
   Exec[download-rstudio] -> Package['rstudio']
+
+  File['/root/R.setup'] -> Exec['setup-R']
+  File['/usr/lib64'] -> Exec['setup-R']
+  Package['libcairo2-dev'] -> Exec['setup-R']
+  Package['build-essential'] -> Exec['setup-R']
 }
