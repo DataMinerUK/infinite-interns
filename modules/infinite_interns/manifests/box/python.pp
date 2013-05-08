51ed46c173b8ee047359aad545c4c4a80b76b757
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2013-05-08 23:46:12 +0100

    Update python build to use pip.

--- modules/infinite_interns/manifests/box/python.pp
+++ modules/infinite_interns/manifests/box/python.pp
@@ -1,13 +1,27 @@
 # Python Infinite Intern
 class infinite_interns::box::python {
 
+  require python::base
+
   package {
     [
-      'ipython',
-      'ipython-notebook',
-      'python-setuptools',
+      'virtualenv',
       'python-dateutil',
-      'python-bs4'
+      'requests',
+      'messytables',
+      'beautifulsoup4'
+    ]: ensure   => latest,
+       provider => 'pip';
+  }
+}
+
+class infinite_interns::box::python::base {
+  package {
+    [
+      'python-pip',
+      'python-dev',
+      'ipython',
+      'ipython-notebook'
     ]: ensure => latest;
   }
 }
