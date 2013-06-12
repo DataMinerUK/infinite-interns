4659cbdd7967601393a0b4fc24b85ec407fe19c2
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2013-06-12 12:39:02 +0100

    Add lxml to python and pandas boxes.

--- modules/infinite_interns/manifests/box/python.pp
+++ modules/infinite_interns/manifests/box/python.pp
@@ -2,6 +2,7 @@
 class infinite_interns::box::python {
 
   require python::base
+  require gcc
 
   package {
     [
@@ -10,10 +11,21 @@ class infinite_interns::box::python {
       'requests',
       'messytables',
       'beautifulsoup4',
-      'kales'
+      'kales',
+      'lxml'
     ]: ensure   => latest,
        provider => 'pip';
   }
+
+  package {
+    [
+      'libxml2-dev',
+      'libxslt-dev'
+    ]: ensure => latest;
+  }
+
+  Package[libxml2-dev] -> Package[lxml]
+  Package[libxslt-dev] -> Package[lxml]
 }
 
 class infinite_interns::box::python::base {
