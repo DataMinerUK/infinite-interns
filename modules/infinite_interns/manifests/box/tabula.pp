6e178b1fb3df2607de8bff8c2a65223264cc9bc4
Daithi O Crualaoich <daithi.ocrualaoich@gmail.com>
2014-01-14 20:45:50 +0000

    Install tabula globally.

--- modules/infinite_interns/manifests/box/tabula.pp
+++ modules/infinite_interns/manifests/box/tabula.pp
@@ -4,10 +4,10 @@ class infinite_interns::box::tabula {
   require jruby
 
    exec {
-    'install-tabula':
-       command => '/usr/local/rvm/rubies/jruby-1.7.9/bin/jruby -S gem install tabula-extractor',
+    'install-tabula-globally':
+       command => '/usr/local/rvm/bin/rvm @global do /usr/local/rvm/rubies/jruby-1.7.9/bin/jruby -S gem install tabula-extractor',
        cwd     => '/root',
-       creates => '/usr/local/rvm/gems/jruby-1.7.9/bin/tabula',
+       creates => '/usr/local/rvm/gems/jruby-1.7.9@global/bin/tabula',
        timeout => 0;
    }
 }
